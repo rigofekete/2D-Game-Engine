34973a8780417a7e806960256d6c9d6d02862cf8
rigofekete <fabricecosteira@gmail.com>
2025-09-24 11:50:02 +0200

    Delete mocho/misc/vscode.bat

--- mocho/misc/vscode.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM when using the commnd vscode.bat, open vs code in the specified directory
-
-"C:\Users\fabri\AppData\Local\Programs\Microsoft VS Code\Code.exe" c:\Engine\mocho\code\
-
-
-
